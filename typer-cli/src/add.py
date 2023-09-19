ced0fb82fc87ff3e417df47a6ce66225ae9785bd
Daniel Macuare <danielmacuare.uk@gmail.com>
2023-09-20 00:35:55 +0100

    add.py: Removing unnecessary comments

--- typer-cli/src/add.py
+++ typer-cli/src/add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-    Source Code to create commands and sub-commands with Typer
+    Add Command: Used to add --discovered-devices or a --single-device to Netbox
 
     Usage:
         python main.py add --help
@@ -9,15 +9,6 @@
         python main.py add --single-device chi-leaf-04
         python main.py add -d
         python main.py add -s chi-leaf-04
-
-Links:
-- Typer Commands: https://typer.tiangolo.com/tutorial/commands/
-- Breaking Down CLI in Multiple Files: https://typer.tiangolo.com/tutorial/subcommands/add-typer/
-- Callback invoke.. https://github.com/tiangolo/typer/issues/119#issuecomment-795654576
-- Why add a callback: https://github.com/tiangolo/typer/issues/402
-
-- "..." use the ellipsis in the Option when you want to have an optional to be required
-but you don't want to provide a default value.
 """
 
 from typer import Option, Typer, echo
