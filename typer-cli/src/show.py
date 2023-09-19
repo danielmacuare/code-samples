8d1daa4461d1f0b9ecf8ae9f1251961f79e274d0
Daniel Macuare <danielmacuare.uk@gmail.com>
2023-09-19 22:53:50 +0100

    show.py: Updating file

--- typer-cli/src/show.py
+++ typer-cli/src/show.py
@@ -1,39 +1,39 @@
 #!/usr/bin/env python3
 
 """
-    Source Code to create commands and sub-commands with Typer
+    Source Code to create commands and arguments with Typer
     Breaking Down CLI in Multiple Files: https://typer.tiangolo.com/tutorial/subcommands/add-typer/
     Typer Commands: https://typer.tiangolo.com/tutorial/commands/
 
     Usage:
-        python show.py show --help
-        python show.py show netbox --help
-        python show.py show netbox devices
-        python show.py show netbox subnets
-        python show.py show discovered --help
-        python show.py show discovered devices
-        python show.py show discovered subnets
+        python main.py show --help
+        python main.py show netbox --help
+        python main.py show netbox devices
+        python main.py show netbox subnets
+        python main.py show local --help
+        python main.py show local devices
+        python main.py show local subnets
 """
 
 from typer import Argument, Typer, echo
 
-app = Typer(short_help='Options: "Discovered" or "Netbox"')
+app = Typer(short_help='Options: "Local" or "Netbox"')
 
 
-@app.command("discovered")
-def show_discovered(
-    scope: str = Argument(help='Choose a scope: "subnet" OR "devices"'),
+@app.command("local")
+def show_local(
+    scope: str = Argument(help='Choose a scope: "subnets" OR "devices"'),
 ) -> None:
-    """Show Devices Discovered"""
-    echo(f"show discovered {scope.lower()}")
+    """Show devices and subnets available in the local DB"""
+    echo(f"Show {scope} available in the local Database")
 
 
 @app.command("netbox")
 def show_netbox(
-    scope: str = Argument(help='Choose a scope: "subnet" OR "devices"'),
+    scope: str = Argument(help='Choose a scope: "subnets" OR "devices"'),
 ) -> None:
-    """Show Devices in Netbox"""
-    echo(f"show netbox {scope.lower()}")
+    """Show devices and subnets available in Netbox"""
+    echo(f"Show {scope} available in Netbox")
 
 
 if __name__ == "__main__":
